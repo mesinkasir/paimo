04c0e9120a6214d172ff55136755b413e1928a41
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2023-03-15 07:34:08 +0700

    Update home.php

--- php/home.php
+++ php/home.php
@@ -1,7 +1,27 @@
 <div class="container-fluid">
 <div class="row">
 		
-
+<?php if (pluginActivated('pluginSearch')): ?>
+<div class="form-inline d-block">
+<input id="search-input" class="form-control mr-sm-2" type="search" placeholder="<?php $language->p('Search') ?>" aria-label="Search">
+<button class="btn btn-outline-dark my-2 my-sm-0" type="button" onClick="searchNow()"><?php $language->p('Search') ?></button>
+<script>
+function searchNow() {
+var searchURL = "<?php echo Theme::siteUrl(); ?>search/";
+window.open(searchURL + document.getElementById("search-input").value, "_self");
+}
+document.getElementById("search-input").onkeypress = function(e) {
+if (!e) e = window.event;
+var keyCode = e.keyCode || e.which;
+if (keyCode == '13') {
+searchNow();
+return false;
+}
+}
+</script>
+</div>
+<?php endif ?>
+	
 <?php if (empty($content)): ?>
 	<div class="text-center p-4">
 	<?php $language->p('No pages found') ?>
